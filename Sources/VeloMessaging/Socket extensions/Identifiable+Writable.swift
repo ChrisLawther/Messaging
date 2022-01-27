f29f0d0811f7ee57d7f2b1c0b986bd83200848d4
Chris <chris@intriguingdevelopments.com>
2022-01-27 21:01:06 +0000

    Typed reception of Protobuf messages

--- Sources/VeloMessaging/Socket extensions/Identifiable+Writable.swift
+++ Sources/VeloMessaging/Socket extensions/Identifiable+Writable.swift
@@ -1,4 +1,5 @@
 import SwiftProtobuf
+import Foundation
 
 extension SwiftProtobuf.Message {
     static var identifier: Data { String(describing: Self.self).data(using: .utf8)! }
@@ -7,5 +8,21 @@ extension SwiftProtobuf.Message {
 extension WriteableSocket {
     func send<T: SwiftProtobuf.Message>(_ msg: T) throws {
         try send([T.identifier, msg.serializedData()])
+    }
+}
+
+struct ProtobufDecodingError: Error, LocalizedError {
+    let expectedIdentifier: Data
+    let actualIdentifier: Data
+}
+
+extension ReadableSocket {
+    func receive<T: SwiftProtobuf.Message>() throws -> T {
+        let data = try receiveMultipartMessage()
+        guard data[0] == T.identifier else {
+            throw ProtobufDecodingError(expectedIdentifier: T.identifier,
+                                        actualIdentifier: data[0]
+        }
+        return try T.init(serializedData: data[1])
     }
 }
