f32504d6e6a66ab7c4854c95a737637c3bb90b74
Chris <chris@intriguingdevelopments.com>
2022-01-27 21:06:02 +0000

    Extensions need to be public, tests need sample messages

--- Sources/VeloMessaging/Socket extensions/Identifiable+Writable.swift
+++ Sources/VeloMessaging/Socket extensions/Identifiable+Writable.swift
@@ -1,27 +1,28 @@
-import SwiftProtobuf
 import Foundation
+import SwiftProtobuf
+import SwiftZeroMQ
 
-extension SwiftProtobuf.Message {
+public extension SwiftProtobuf.Message {
     static var identifier: Data { String(describing: Self.self).data(using: .utf8)! }
 }
 
-extension WriteableSocket {
+public extension WriteableSocket {
     func send<T: SwiftProtobuf.Message>(_ msg: T) throws {
         try send([T.identifier, msg.serializedData()])
     }
 }
 
-struct ProtobufDecodingError: Error, LocalizedError {
+public struct ProtobufDecodingError: Error, LocalizedError {
     let expectedIdentifier: Data
     let actualIdentifier: Data
 }
 
-extension ReadableSocket {
+public extension ReadableSocket {
     func receive<T: SwiftProtobuf.Message>() throws -> T {
         let data = try receiveMultipartMessage()
         guard data[0] == T.identifier else {
             throw ProtobufDecodingError(expectedIdentifier: T.identifier,
-                                        actualIdentifier: data[0]
+                                        actualIdentifier: data[0])
         }
         return try T.init(serializedData: data[1])
     }
