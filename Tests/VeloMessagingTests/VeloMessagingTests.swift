48ea7b67731b77920d7879539eb2a6ad91a4c018
Chris <chris@intriguingdevelopments.com>
2022-02-15 21:19:25 +0000

    Removes redundant extensions from test target

--- Tests/VeloMessagingTests/VeloMessagingTests.swift
+++ Tests/VeloMessagingTests/VeloMessagingTests.swift
@@ -3,27 +3,7 @@ import SwiftZeroMQ
 import SwiftProtobuf
 @testable import VeloMessaging
 
-extension SwiftProtobuf.Message {
-    static var identifier: Data { String(describing: Self.self).data(using: .utf8)! }
-}
-
-extension WriteableSocket {
-    func send<T: SwiftProtobuf.Message>(_ msg: T) throws {
-        try send([T.identifier, msg.serializedData()])
-    }
-}
-
 struct SomeError: Error {}
-
-extension ReadableSocket {
-    func receive<T: SwiftProtobuf.Message>() throws -> T {
-        let data = try receiveMultipartMessage()
-        guard data[0] == T.identifier else {
-            throw SomeError()
-        }
-        return try T.init(serializedData: data[1])
-    }
-}
 
 final class VeloMessagingTests: XCTestCase {
     var ctx: ZMQ!
