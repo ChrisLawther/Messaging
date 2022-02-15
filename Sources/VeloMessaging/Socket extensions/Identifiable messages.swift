467e39bb181e448b14db13520d027a64fb189aa5
Chris <chris@intriguingdevelopments.com>
2022-02-15 18:35:53 +0000

    Adopts implementation of on<T>, adds some doc-comments

---
+++ Sources/VeloMessaging/Socket extensions/Identifiable messages.swift
@@ -0,0 +1,59 @@
+import Foundation
+import SwiftProtobuf
+import SwiftZeroMQ
+
+
+/// Thrown when/if the identifiet of a received message does not match the expected identifier
+public struct ProtobufDecodingError: Error, LocalizedError {
+    let expectedIdentifier: Data
+    let actualIdentifier: Data
+}
+
+// MARK: - Identifying messages by their type
+
+public extension SwiftProtobuf.Message {
+    /// String representation of the message type
+    static var identifier: Data {
+        String(describing: Self.self).data(using: .utf8)!
+    }
+}
+
+// MARK: - Sending typed, identified messages
+
+public extension WriteableSocket {
+    /// Sends the provided message as a two-part message comprising the identifier followed by the serialized message
+    /// - Parameter msg: The message to send
+    func send<T: SwiftProtobuf.Message>(_ msg: T) throws {
+        try send([T.identifier, msg.serializedData()])
+    }
+}
+
+// MARK: - Receiving typed, identified messages
+
+public extension ReadableSocket {
+    /// Receive a typed, identified message
+    /// - Returns: A protobuf message
+    /// - Throws: When the payload could not be decoded to the expected type
+    func receive<T: SwiftProtobuf.Message>() throws -> T {
+        let data = try receiveMultipartMessage()
+        guard data[0] == T.identifier else {
+            throw ProtobufDecodingError(expectedIdentifier: T.identifier,
+                                        actualIdentifier: data[0])
+        }
+        return try T.init(serializedData: data[1])
+    }
+}
+
+public extension ReadableSocket {
+    /// Register a handler for a specific protobuf message type
+    /// - Parameter type: The message type of interest
+    /// - Parameter handler: Closure to handle any received messages
+    func on<T: SwiftProtobuf.Message>(_ type: T.Type = T.self, handler: @escaping (T) -> Void) -> Void {
+        on(type.identifier) { data in
+            guard let message = try? T.init(serializedData: data[0]) else {
+                return
+            }
+            handler(message)
+        }
+    }
+}
